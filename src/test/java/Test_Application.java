c59188c3f6745beec57073aec6f2bc9f5f52db1c
arund <arun.dark@gmail.com>
2022-01-31 12:42:09 +0000

    Jira -commit to repo

---
+++ src/test/java/Test_Application.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class Test_Application {
+
+    @Test
+
+    public void test1() {
+        System.out.println(" Running Test 1");
+    }
+
+@Test
+    public void test2() {
+
+    System.out.println(" Running Test 2");
+}
+
+}
